f88e24bdb0d140d9d5c811f32be0e4e53a1653aa
muhamedcakaj <muhamedcakaj57@gmail.com>
2025-01-27 19:26:07 +0100

    Adding Logs Crud

---
+++ EventTicketing/Areas/Admin/Controllers/AdminLogsController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EventTicketing.Data;
+using EventTicketing.Models;
+
+namespace EventTicketing.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminLogsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdminLogsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/AdminLogs
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Logs.ToListAsync());
+        }
+
+        // GET: Admin/AdminLogs/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var log = await _context.Logs
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (log == null)
+            {
+                return NotFound();
+            }
+
+            return View(log);
+        }
+
+        // GET: Admin/AdminLogs/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/AdminLogs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Action,UserId,CreatedAt")] Log log)
+        {
+           
+                _context.Add(log);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+          
+        }
+
+        // GET: Admin/AdminLogs/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var log = await _context.Logs.FindAsync(id);
+            if (log == null)
+            {
+                return NotFound();
+            }
+            return View(log);
+        }
+
+        // POST: Admin/AdminLogs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Action,UserId,CreatedAt")] Log log)
+        {
+            if (id != log.Id)
+            {
+                return NotFound();
+            }
+
+         
+                    _context.Update(log);
+                    await _context.SaveChangesAsync();
+             
+            
+            return View(log);
+        }
+
+        // GET: Admin/AdminLogs/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var log = await _context.Logs
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (log == null)
+            {
+                return NotFound();
+            }
+
+            return View(log);
+        }
+
+        // POST: Admin/AdminLogs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var log = await _context.Logs.FindAsync(id);
+            if (log != null)
+            {
+                _context.Logs.Remove(log);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LogExists(int id)
+        {
+            return _context.Logs.Any(e => e.Id == id);
+        }
+    }
+}
